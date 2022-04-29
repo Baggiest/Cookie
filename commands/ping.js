e04dc55892f08c1f83ec33adb942a1d688848dbf
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-04-29 18:11:11 +0430

    webhook test

--- commands/ping.js
+++ commands/ping.js
@@ -8,6 +8,7 @@ module.exports = {
       `Pong! Latency is \`${
         m.createdTimestamp - message.createdTimestamp
       }ms\`. API Latency is \`${message.client.ws.ping} 🍪 ms\``
+
     );
   },
 };
