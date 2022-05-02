49bf83870b83a4ad52ac415a7e2614dd9c6c40ec
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-05-02 18:18:24 +0430

    done signup and balance done

---
+++ commands/balance.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+const Handler = require("../mongo/handler")
+const caller = "balance module"
+const handler = new Handler(caller)
+
+module.exports = {
+    name: "balance", 
+    description: "for checking your cookie balance",
+
+    async execute(message){
+        await handler.getBalance(message)
+    }
+}
