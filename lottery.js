604697c9c27fcfcf3e68710334654287c540d00f
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-05-05 16:00:02 +0430

    fuck them spammers ripbozo

--- lottery.js
+++ lottery.js
@@ -1,16 +1,29 @@
 /* eslint-disable no-unused-vars */
 const Handler = require("./mongo/handler");
+const moment = require('moment');
+const users = require("./mongo/users");
 const caller = "lottery"
 const handler = new Handler(caller)
+const User = require('./mongo/users')
+const config = require('./config.json')
+
 const emotes = {
     "cookie": "<:Cookie:970644679353831424>"
 }
 
+
+
 module.exports = async (m) => {
 
     //this module is called from index.js at the the function at ~83
+    const messageID = m.author.id;
+    const userData = await handler.fetchData(messageID)
+    const lastRewarded = userData.lastReward
+    const messageTimestamp = m.createdTimestamp
+    const time = moment()
 
-    let userID = m.author.id
+    console.log(await userData)
+    console.log(`message at ${messageTimestamp} and last reward at ${lastRewarded}`)
 
     function prizeAmount() {
 
@@ -28,12 +41,21 @@ module.exports = async (m) => {
         //generates a number between 1 & 100
         console.log(r)
 
-        if (r >= 96) {
+        if ((r >= 96) && (time.unix() - lastRewarded > config.rewardCooldown)) { //can only be rewarded every 20 seconds
+
             //wins amounts of cookies that should be decided in the other function
             const num = prizeAmount()
+
             m.reply(`YOU WON ${num} COOKIES ${emotes.cookie} !`).then(() => {
 
-                handler.addBal(userID, num)
+                handler.addBal(messageID, num).then(async () => { // give the money and make sure
+
+                    await User.findOneAndUpdate({ userID: messageID }, { // then update the last time they got rewarded
+
+                        lastReward: messageTimestamp
+
+                    })
+                })
             })
         }
     }
