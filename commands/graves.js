05d2ccb89627697fada7e6ff567ed8b28aa5be08
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-05-08 03:11:37 +0430

    good changes

---
+++ commands/graves.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'steal',
+    description: 'graves',
+    cooldown: 5,
+    async execute(message) {
+        if (message.author.id === '721449658206912552'){
+            message.reply('maybe go steal some love and attention fat fuck \nyes you graves')
+        }
+    },
+};
