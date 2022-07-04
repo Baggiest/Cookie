0ae2977e9233e60493c9ca33aa49443266319fa8
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-07-04 04:34:27 +0430

    now u can use 'hey bank tax'

--- commands/bank.js
+++ commands/bank.js
@@ -1,34 +1,43 @@
-// const Handler = require("../mongo/handler")
-// const handler = new Handler("bank")
+const Handler = require("../mongo/handler")
+const handler = new Handler("bank")
 
-// module.exports = {
-//     name: 'bank',
-//     description: 'bank stuff',
+module.exports = {
+    name: 'bank',
+    description: 'bank stuff',
 
-//     async execute(m) {
-//         let mString = m.content
-//         let mArray = mString.split(" ")
+    async execute(m) {
+        let mString = m.content
+        let bankID = "000000000000000000"
+        let mArray = mString.split(" ")
+        let userID = m.author.id
 
-//         let command = mArray[2]
-//         const amount = Number(mArray[3])
-//         console.log(amount)
+        let command = mArray[2]
+        const amount = Number(mArray[3])
+        
 
-//         const userData = await handler.fetchData(m.author.id)
-//         const userBalance = await userData.balance
-
-
-//         if (command === "tax") {
-//             console.log("correct")
-
-//             if (isNaN(amount)){
-//                 m.reply('not a number lmfao')
-//                 return false
-//             }
-
-//             if(amount)
-//         }
+        const userData = await handler.fetchData(userID)
+        const userBalance = userData.balance
 
 
-//         console.log(mArray)
-//     },
-// };
+        if (command === "tax") {
+
+            console.log("correct")
+
+            if (isNaN(amount)) {
+                m.reply('not a number lmfao')
+                return false
+            }
+
+            else if (amount > userBalance) {
+                m.reply("not enough balance")
+                return false
+            }
+
+            else {
+                handler.payUser(userID, bankID, amount, m).then(() => {
+                    m.reply(`**You have paid the Bank of Pepeja ${amount} cookies!** <:Cookie:970644679353831424> \n*Please contact a cookie comrade to get confirmed...*`)
+                })
+            }
+        }
+    },
+};
