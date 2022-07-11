2b2e864bbdab37da98da4d8e2da2ef2813444a67
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-07-11 03:12:21 +0200

    (Cookie Bot): Fixed the trigger function ooops

--- lottery.js
+++ lottery.js
@@ -4,7 +4,9 @@ const caller = "lottery"
 const handler = new Handler(caller)
 const User = require('./mongo/users')
 const config = require('./config.json');
-const { delta } = require("ccxt");
+const {
+    delta
+} = require("ccxt");
 
 const emotes = {
     "cookie": "<:Cookie:970644679353831424>",
@@ -42,76 +44,80 @@ module.exports = async (m) => {
         //generates a number between 1 & 100
         console.log(r)
 
-
         //console.log(userIsBanned)
         console.log("message", mTime, "lastRew", lastRewarded, "delta", `${mTime - lastRewarded}`)
 
-        if (r <= 22) {
-            let lotteryMsg = m.send(`THE COOKIE LOTTORY HAS BEEN STARTED! ${emotes.cookie}\n\n` + `React to win up to 15 cookies!`)
-            await lotteryMsg.react(emotes.giveCookie)
+        if (r >= 99) {
+            let newNumber = Math.floor(Math.random() * 100) + 1;
 
-            // sleep for 60 seconds
-            await sleep(60000)
+            // 20% chance of activting the lottery
+            if (newNumber >= 1 && newNumber <= 20) {
+                let lotteryMsg = m.send(`THE COOKIE LOTTORY HAS BEEN STARTED! ${emotes.cookie}\n\n` + `React to win up to 15 cookies!`)
+                await lotteryMsg.react(emotes.giveCookie)
 
-            // get all users who reacted to the message
-            let users = await lotteryMsg.reactions.cache.get(emotes.giveCookie.id).users.fetch()
+                // sleep for 60 seconds
+                await sleep(60000)
 
-            // filter out the bots and make an array of the users and filter if user is not banned
-            let usersArray = users.filter(user => !user.bot && !isBanned)
-            console.log(usersArray)
+                // get all users who reacted to the message
+                let users = await lotteryMsg.reactions.cache.get(emotes.giveCookie.id).users.fetch()
 
-            // if there are users who reacted to the message
-            if (usersArray.size > 0) {
+                // filter out the bots and make an array of the users and filter if user is not banned
+                let usersArray = users.filter(user => !user.bot && !isBanned)
+                console.log(usersArray)
 
-                // pick a random user from the array
-                let winner = usersArray[Math.floor(Math.random() * usersArray.length)]
-                console.log(winner)
+                // if there are users who reacted to the message
+                if (usersArray.size > 0) {
 
-                // get a random amount up to 25 cookies 
-                let prize = Math.floor(Math.random() * 25) + 1;
+                    // pick a random user from the array
+                    let winner = usersArray[Math.floor(Math.random() * usersArray.length)]
+                    console.log(winner)
 
-                // send a message to the winner
-                let winnerMsg = await m.send(`${winner.username} won ${prize} cookies from the lottery! ${emotes.giveCookie}`) 
+                    // get a random amount up to 25 cookies 
+                    let prize = Math.floor(Math.random() * 25) + 1;
 
-                handler.addBal(winner, num).then(async () => {
-                    try {
-                        await User.findOneAndUpdate({
-                            userID: winner,
-                        }, {
-                            // then update the last time they got rewarded
-                            lastReward: mTime,
-                        })
-                    } catch (error) {
-                        console.log(error)
-                    }
-                })
-            }
-        }
-        
-        else 
-        {
-            if ((r >= 99) && (mTime - lastRewarded > 400) && (isBanned === false)) { //can only be rewarded every 20 seconds
+                    // send a message to the winner
+                    let winnerMsg = await m.send(`${winner.username} won ${prize} cookies from the lottery! ${emotes.giveCookie}`)
 
-                //wins amounts of cookies that should be decided in the other function
-                const num = prizeAmount()
+                    handler.addBal(winner, num).then(async () => {
+                        try {
+                            await User.findOneAndUpdate({
+                                userID: winner,
+                            }, {
+                                // then update the last time they got rewarded
+                                lastReward: mTime,
+                            })
+                        } catch (error) {
+                            console.log(error)
+                        }
+                    })
+                }
+            } 
+            
+            else 
+            {
+                if ((mTime - lastRewarded > 400) && (isBanned === false)) { //can only be rewarded every 20 seconds
 
-                console.log(msgAuthorID)
-                m.reply(`YOU WON ${num} COOKIES ${emotes.cookie} !`)
+                    //wins amounts of cookies that should be decided in the other function
+                    const num = prizeAmount()
 
-                handler.addBal(msgAuthorID, num).then(async () => { // give the money and make sure
+                    console.log(msgAuthorID)
+                    m.reply(`YOU WON ${num} COOKIES ${emotes.cookie} !`)
 
-                    try {
-                        await User.findOneAndUpdate({
-                            userID: msgAuthorID
+                    handler.addBal(msgAuthorID, num).then(async () => { // give the money and make sure
 
-                        }, {
-                            // then update the last time they got rewarded
-                            lastReward: mTime
-                        })
-                    } catch (error) {
-                        console.log(error)
-                    }
-                })
+                        try {
+                            await User.findOneAndUpdate({
+                                userID: msgAuthorID
+
+                            }, {
+                                // then update the last time they got rewarded
+                                lastReward: mTime
+                            })
+                        } catch (error) {
+                            console.log(error)
+                        }
+                    })
+                }
             }
         }
     }
