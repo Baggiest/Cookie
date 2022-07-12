1f978678514697a892cb591de80a8b0c8747171a
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-07-13 02:10:01 +0430

    aigh

---
+++ commands/addicts.js
@@ -0,0 +1,41 @@
+// const Handler = require("../mongo/handler")
+// const handler = new Handler(caller)
+
+const User = require('../mongo/users')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'addicts',
+    description: 'its a leaderboard so these mfs shut the fuck up',
+    cooldown: 5,
+
+    async execute(message) {
+
+        let i;
+        const objects = await User.find({}).limit(15).select('timesPlayed').select('userTag').sort({ timesPlayed: -1 })
+
+        let leaderboard;
+
+        for (i = 0; i < objects.length; i++) {
+
+            //console.log(i + 1, objects[i].balance)
+            let score = objects[i].timesPlayed.toString()
+            let ID = objects[i].userTag
+
+            leaderboard += ` \n ${i + 1}. ***${ID}*** \t ** | ${score}**\t`
+        }
+        console.log(leaderboard)
+
+        const leaderEmbed = new MessageEmbed()
+
+            .setTitle('Lmfao guess game addicts')
+            .setColor('YELLOW')
+            .setThumbnail('https://cdn.discordapp.com/attachments/883245986166759437/972292034012532838/1651882879600.png')
+            .setDescription(leaderboard.replace('undefined', '').replace('1.', '💀') ) // yea fuck you too
+            
+        message.channel.send({ embeds: [leaderEmbed] })
+    },
+
+
+};
+//
